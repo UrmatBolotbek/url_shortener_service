c41d694ca471ab89a3696c1834276fdb3b6aee06
UrmatBolotbek <urmar9797@gmail.com>
2025-03-25 15:56:41 +0100

    some updates

--- src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
+++ src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
@@ -1,9 +1,11 @@
 package faang.school.urlshortenerservice;
 
+import faang.school.urlshortenerservice.config.properties.UrlLifeTimeConfig;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
@@ -14,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableRabbit
 @EnableRetry
+@EnableConfigurationProperties(UrlLifeTimeConfig.class)
 @EnableFeignClients("school.faang.servicetemplate.client")
 public class ServiceTemplateApplication {
     public static void main(String[] args) {
--- src/main/java/faang/school/urlshortenerservice/config/properties/UrlLifeTimeConfig.java
+++ src/main/java/faang/school/urlshortenerservice/config/properties/UrlLifeTimeConfig.java
@@ -1,16 +1,20 @@
 package faang.school.urlshortenerservice.config.properties;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
-@Data
-@Builder
-@Component
+@Getter
 @ConfigurationProperties(prefix = "app.url.life-time")
 public class UrlLifeTimeConfig {
-    private int months;
-    private int days;
-    private int hours;
+    private final int months;
+    private final int days;
+    private final int hours;
+
+    @ConstructorBinding
+    public UrlLifeTimeConfig(int months, int days, int hours) {
+        this.months = months;
+        this.days = days;
+        this.hours = hours;
+    }
 }
